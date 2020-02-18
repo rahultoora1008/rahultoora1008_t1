d08f573892bac1f66605c7bf84448dff52ad60b6
rahultoora1008 <rahultoora1008@gmail.com>
2020-02-18 11:55:57 -0500

    resopnsive mobile size changed

--- src/components/main.js
+++ src/components/main.js
@@ -9,12 +9,12 @@ class Main extends Component {
 					<h1>
 						<span>Hi, </span>
 						<span>my name is,</span>
-						<span className="myName">Rahul</span>
+						<span className="myName" title="I know hard to Pronounce">Rahul</span>
 						<span>I'm a learning<span className="devLandscape">Developer</span></span>
 						<span className="devPortrait">Developer</span>
 					</h1>
 					<p>MongoDB / Express / NodeJS / React / ...</p>
-					<a href="#resume">Resume</a>
+					<a href="#resume" title="I know">Resume</a>
 				</div>
 				<div className="main-section-img" />
 			</div>
