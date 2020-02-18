1a01549a4bc9fcbf837c4bc8c06ca1c9327610ab
rahultoora1008 <rahultoora1008@gmail.com>
2020-02-18 10:24:36 -0500

    responsive design done

--- src/components/main.js
+++ src/components/main.js
@@ -7,11 +7,11 @@ class Main extends Component {
 			<div className="container main-section">
 				<div className="text">
 					<h1>
-						<div>Hi,</div>
-						<div>
-							I'm <span className="myName">Rahul</span>
-						</div>
-						<div>a learningDeveloper</div>
+						<span>Hi, </span>
+						<span>my name is,</span>
+						<span className="myName">Rahul</span>
+						<span>I'm a learning<span className="devLandscape">Developer</span></span>
+						<span className="devPortrait">Developer</span>
 					</h1>
 					<p>MongoDB / Express / NodeJS / React / ...</p>
 					<a href="#resume">Resume</a>
